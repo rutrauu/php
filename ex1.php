90da2dbc2bb35647b55d865542741e511cbb729c
Artur Raguse <166300088+rutrauu@users.noreply.github.com>
2025-03-28 21:53:45 -0300

    Add files via upload

---
+++ ex1.php
@@ -0,0 +1,15 @@
+<?php
+
+    for ($i=1; $i <= 6; $i++) { 
+        echo "<h$i> Titulo $i </h$i>";
+    }
+
+    echo "<hr>";
+
+    $i = 1;
+    while ($i <= 6) {
+        echo "<h$i> Titulo $i </h$i>";
+        $i++;
+    }
+    
+?>
